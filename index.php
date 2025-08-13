94376df9b8841faf7e7e75349e9b59f57eefb8fe
Pravin Singh Rana <dev.psrana@gmail.com>
2025-08-13 10:26:32 +0530

    added custom post type

--- index.php
+++ index.php
@@ -13,7 +13,7 @@ add_menu_page($page_title, $menu_title, $capability, $menu_slug, $function, $ico
 
 # calling the admin_menu action hook with the function where i have defined the settings 
 add_action('admin_menu', 'function_to_call');
-function function_to_call(
+function function_to_call(){
     #calling the real function
 add_menu_page(
     'MY Plugin settings page',#visible in top bar <title>
@@ -31,10 +31,9 @@ add_submenu_page(
     'manage-options', #capability 
     'my-submenu-settings',#slug
     'my-submenu-function'#callback function
-)
-
 );
 
+}
 # adding option page in settings 
 
 add_action('admin_menu','add_option page');
@@ -79,3 +78,19 @@ $args = [
 ]
 
 register_options('option group','option name','args');
+
+// custom post type CPT
+
+public function custom_post_type(){
+   regirster_post_type('nameofposttype',[
+    'labels'=>[
+        'name'=>__('Products','text-domain'),
+        'singular_name'=>__('Product','text_domain')
+    ],
+    'public'=>true,
+    'has_archive'=>true
+   ])
+
+}
+// now its time to register 
+add_action('init','custom_post_type');

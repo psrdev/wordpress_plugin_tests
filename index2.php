d14caab7fa25ca27bbbdb2d98f3d499f1078f65e
Pravin Singh Rana <dev.psrana@gmail.com>
2025-08-21 20:13:12 +0530

    reverse  string

---
+++ index2.php
@@ -0,0 +1,16 @@
+<?php
+// reverse the string 
+// function reverseString(string $text): string
+// {
+//     if (strlen($text) === 0) {
+//         return '';
+//     }
+//     return reverseString(substr($text, 1)) . $text[0];
+
+// }
+
+// $str = "start";
+// echo reverseString($str);
+
+// luhan farmula 
+
